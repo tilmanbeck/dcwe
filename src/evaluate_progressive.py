a32c2b3fa2e68de504ccf5024dd3169dac25a42d
Tilman Beck <beck@ukp.informatik.tu-darmstadt.de>
2022-01-05 21:59:57 +0100

    evaluation

--- src/evaluate_progressive.py
+++ src/evaluate_progressive.py
@@ -7,22 +7,28 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--data_name", type=str, required=True)
 parser.add_argument("--dir", type=str, required=True)
 
-args = parser.parse_args()
-result_dirs = os.listdir(args.dir)
-eval_results = []
-test_results = []
 
-for d in result_dirs:
-    with open(os.path.join(args.dir, d, 'eval_results.json')) as fp:
-        eval_result = json.load(fp)
-        eval_results.append(eval_result[metrics_for_datasets[args.data_name]])
-    with open(os.path.join(args.dir, d, 'test_results.json')) as fp:
-        test_result = json.load(fp)
-        test_results.append(test_result[metrics_for_datasets[args.data_name]])
+def evaluate_progressive():
+	args = parser.parse_args()
+	result_dirs = os.listdir(args.dir)
+	eval_results = []
+	test_results = []
 
-print('Results for: ', args.dir)
-print('Metric: ', metrics_for_datasets[args.data_name])
-print('eval mean (std): {:.4f} ({:.4f})'.format(np.mean(eval_results), np.std(eval_results)))
-print('test mean (std): {:.4f} ({:.4f})'.format(np.mean(test_results), np.std(test_results)))
+	for d in result_dirs:
+		with open(os.path.join(args.dir, d, 'eval_results.json')) as fp:
+        		eval_result = json.load(fp)
+        		eval_results.append(eval_result['eval_' + metrics_for_datasets[args.data_name]])
+	with open(os.path.join(args.dir, d, 'test_results.json')) as fp:
+		test_result = json.load(fp)
+		test_results.append(test_result['test_' + metrics_for_datasets[args.data_name]])
+	
+	print('Results for: ', args.dir)
+	print('Metric: ', metrics_for_datasets[args.data_name])
+	print('eval mean (std): {:.4f} ({:.4f})'.format(np.mean(eval_results), np.std(eval_results)))
+	print('test mean (std): {:.4f} ({:.4f})'.format(np.mean(test_results), np.std(test_results)))
+
+def evaluate_temporal_stratified():
+
+
 
 
